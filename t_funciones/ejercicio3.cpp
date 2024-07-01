84fe10b8e14ae1e2189900a07e21ee2bcb26ae35
AronQuito <acachicatariq@unjbg.edu.pe>
2024-06-30 22:39:10 -0500

    Actualiza ejercicio 3: Transformacion a funcion

--- t_funciones/ejercicio3.cpp
+++ t_funciones/ejercicio3.cpp
@@ -1,10 +1,15 @@
 #include "iostream"
 #include "cmath"
 using namespace std;
+int hip(int a, int b){
+    int r;
+    r=sqrt(pow(a,2)+pow(b,2));
+    return r;
+}
 int main(){
     int c1,c2,h;
     cout<<"Introduce el cateto 1: ";cin>>c1;
     cout<<"Introduce el cateto 2: ";cin>>c2;
-    cout<<"La hipotenusa es: "<<sqrt(pow(c1,2)+pow(c2,2));
+    cout<<"La hipotenusa es: "<<hip(c1,c2);
     return 0;
 }
